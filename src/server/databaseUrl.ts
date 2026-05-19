94021f743ad587ec12afac779bf0e806577c3bb0
Knightflare15 <knightflare2004@gmail.com>
2026-05-19 14:41:13 +0530

    removed exposed password

--- src/server/databaseUrl.ts
+++ src/server/databaseUrl.ts
@@ -1,7 +1,4 @@
-const FALLBACK_DATABASE_URL =
-  "sqlserver://tmason-server.database.windows.net:1433;database=tmason_db;user=tmasonadmin;password=Flare@101271;encrypt=true;trustServerCertificate=false";
-
 export function getDatabaseUrl(): string | undefined {
   const configuredUrl = process.env.DATABASE_URL?.trim();
-  return configuredUrl || FALLBACK_DATABASE_URL;
+  return configuredUrl || undefined;
 }
